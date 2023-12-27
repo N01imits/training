2ed026d512faf201efcd27ef9536eeced02f1565
Ilya Belyakov <thesamecrazyman@gmail.com>
2023-12-27 15:38:50 +0300

    add modal

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,33 @@ window.addEventListener('DOMContentLoaded', () => {
 	const tabs = document.querySelectorAll('.operations__tab');
 	const tabsContainer = document.querySelector('.operations__tab-container');
 	const tabsContent = document.querySelectorAll('.operations__content');
+	const modal = document.querySelector('.modal');
+	const overlay = document.querySelector('.overlay');
+	const btnCloseModal = document.querySelector('.btn--close-modal');
+	const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+	const openModal = e => {
+		e.preventDefault();
+		modal.classList.remove('hidden');
+		overlay.classList.remove('hidden');
+	};
+
+	const closeModal = () => {
+		modal.classList.add('hidden');
+		overlay.classList.add('hidden');
+	};
+
+	btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+	btnCloseModal.addEventListener('click', closeModal);
+	overlay.addEventListener('click', closeModal);
+
+	document.addEventListener('keydown', e => {
+		if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+			closeModal();
+		}
+	});
+
 	tabsContainer.addEventListener('click', function (e) {
 		const clicked = e.target.closest('.operations__tab');
 
