41db69dd627e3b679adb9c81f1c15eb97a55a17b
Ilya Belyakov <darksiders1282@gmail.com>
2024-01-07 14:23:45 +0300

    moved project to typescript

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -7,30 +7,31 @@ window.addEventListener('DOMContentLoaded', () => {
 	const btnCloseModal = document.querySelector('.btn--close-modal');
 	const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-	const openModal = e => {
+	const openModal = (e: Event) => {
 		e.preventDefault();
-		modal.classList.remove('hidden');
-		overlay.classList.remove('hidden');
+		modal?.classList.remove('hidden');
+		overlay?.classList.remove('hidden');
 	};
 
 	const closeModal = () => {
-		modal.classList.add('hidden');
-		overlay.classList.add('hidden');
+		modal?.classList.add('hidden');
+		overlay?.classList.add('hidden');
 	};
 
 	btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-	btnCloseModal.addEventListener('click', closeModal);
-	overlay.addEventListener('click', closeModal);
+	btnCloseModal?.addEventListener('click', closeModal);
+	overlay?.addEventListener('click', closeModal);
 
 	document.addEventListener('keydown', e => {
-		if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+		if (e.key === 'Escape' && !modal?.classList.contains('hidden')) {
 			closeModal();
 		}
 	});
 
-	tabsContainer.addEventListener('click', function (e) {
-		const clicked = e.target.closest('.operations__tab');
+	tabsContainer?.addEventListener('click', function (e) {
+		const target = e.target as HTMLElement;
+		const clicked = target.closest('.operations__tab');
 
 		// Guard clause
 		if (!clicked) return;
@@ -44,8 +45,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		// Activate content area
 		document
-			.querySelector(`.operations__content--${clicked.dataset.tab}`)
-			.classList.add('operations__content--active');
+			?.querySelector(`.operations__content--${clicked.dataset.tab}`)
+			?.classList.add('operations__content--active');
 	});
 
 	// accordion
@@ -76,8 +77,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	const accordionContainer = document.querySelector('.accordion');
 
-	accordionContainer.addEventListener('click', event => {
-		const accordionHeader = event.target.closest('.accordion__header');
+	accordionContainer?.addEventListener('click', (event: Event) => {
+		const target = event.target as HTMLElement;
+		const accordionHeader = target.closest('.accordion__header');
 		if (!accordionHeader) return;
 
 		const accordion = accordionHeader.parentElement;
@@ -86,7 +88,8 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 
 	document.addEventListener('keydown', event => {
-		const accordion = event.target.closest('.accordion__item');
+		const target = event.target as HTMLElement;
+		const accordion = target.closest('.accordion__item');
 
 		if (event.key !== 'Escape') return;
 		if (!accordion) return;
